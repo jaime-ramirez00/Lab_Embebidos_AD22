268aeb32cd68171ead37d3e19aaf6f766b0e2e96
jaime-ramirez00 <60994598+jaime-ramirez00@users.noreply.github.com>
2022-08-31 11:01:17 -0500

    Code added to Rectangle.c

--- Laboratory3/Rectangle.c
+++ Laboratory3/Rectangle.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+      int i=0;
+      //printf("%d",argv[1]);
+      if(!strcmp(argv[1],"-a"))
+      {
+            printf("area =%d units\n", atoi(argv[3])*atoi(argv[5]));
+      }
+      if(!strcmp(argv[1],"-p"))
+      {
+            printf("perimeter =%d units\n", 2*(atoi(argv[3])+atoi(argv[5])));
+      }
+      return 0;
+}
